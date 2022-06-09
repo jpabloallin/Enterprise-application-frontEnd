6fb680f13666b594453ffadb0cbf3269d3ef3eea
jpabloallin <jpac.647@gmail.com>
2022-06-09 16:54:12 -0500

    Deleted unused information on App and added NavBar, Footer, Links and Routes asociate with my pages.

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,27 @@
-import { useState } from 'react'
-import logo from './logo.svg'
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom'
 import './App.css'
+import Footer from './components/Footer/Footer'
+import NavBar from './components/NavBar/NavBar'
+import Main from './components/Main/Main'
+import Provider from './components/Provider/Provider'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
+    <BrowserRouter>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+    <NavBar />
+      <nav className="navbar navbar-dark bg-light d-flex justify-content-around p-1">
+        <Link to="/" className="link-dark btn btn-secondary btn-lg active p-2">Main page</Link>
+        <Link to="/provider" className="btn btn-secondary btn-lg active p-2">Provider</Link>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/provider" element={<Provider/>} />
+      </Routes>
+      <Footer/>
     </div>
+    </BrowserRouter>
   )
 }
 
