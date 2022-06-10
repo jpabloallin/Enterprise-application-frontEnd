df5aa0d916eddec85a3142945984b5fc86973249
jpabloallin <jpac.647@gmail.com>
2022-06-10 15:07:42 -0500

    Created Product page

---
+++ src/components/product/Product.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { productType } from '../../types/productTypes'
+
+interface IProductProps { 
+  product?: productType
+}
+
+const Product:React.FC<IProductProps> = () => {
+  
+  
+  
+  
+  return (
+    <div>product</div>
+  )
+}
+
+export default Product
