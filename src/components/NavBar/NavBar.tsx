c7266f39ea1e6a0b8c9fd6d50fb3c4e5456c6b6f
jpabloallin <jpac.647@gmail.com>
2022-06-09 21:33:27 -0500

    changed main title

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -3,7 +3,7 @@ import '../../App.css'
 const NavBar = () => {
   return (
     <nav className="navbar navbar-dark bg-dark fw-bold fs-1  bg-dark p-3 text-white">
-      <span className="navbar-brand mx-auto">Welcome to my React Application</span>
+      <span className="navbar-brand mx-auto">Don Raul’s Hardware Store</span>
     </nav>
   )
 }
