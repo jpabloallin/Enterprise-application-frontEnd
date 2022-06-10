37702ff4ad6ec3913fd2216d90cf19e74b2edbb5
jpabloallin <jpac.647@gmail.com>
2022-06-10 15:08:31 -0500

    Added Product component to Route and Link components

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import Footer from './components/Footer/Footer'
 import NavBar from './components/NavBar/NavBar'
 import Main from './components/Main/Main'
 import Provider from './components/Provider/Provider'
+import Product from './components/product/product'
 
 function App() {
 
@@ -14,10 +15,12 @@ function App() {
       <nav className="navbar navbar-dark bg-light d-flex justify-content-around p-1">
         <Link to="/" className="link-dark btn btn-secondary btn-lg active p-2">Main page</Link>
         <Link to="/provider" className="link-dark btn btn-secondary btn-lg active p-2">Provider</Link>
+        <Link to="/product" className="link-dark btn btn-secondary btn-lg active p-2">Product</Link>
       </nav>
       <Routes>
         <Route path="/" element={<Main />} />
         <Route path="/provider" element={<Provider/>} />
+        <Route path="/product" element={<Product/>} />
       </Routes>
       <Footer/>
     </div>
