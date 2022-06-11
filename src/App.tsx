230426e5be110d50efd2c817be3959b01efa40fb
jpabloallin <jpac.647@gmail.com>
2022-06-10 23:53:32 -0500

    Created Receipt page nad added to my App

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import NavBar from './components/navBar/NavBar'
 import Main from './components/main/Main'
 import Provider from './components/provider/Provider'
 import Product from './components/product/Product'
+import Receipt from './components/receipt/Receipt'
 
 function App() {
 
@@ -16,11 +17,13 @@ function App() {
         <Link to="/" className="link-dark btn btn-secondary btn-lg active p-2">Main page</Link>
         <Link to="/provider" className="link-dark btn btn-secondary btn-lg active p-2">Provider</Link>
         <Link to="/product" className="link-dark btn btn-secondary btn-lg active p-2">Product</Link>
+        <Link to="/receipt" className="link-dark btn btn-secondary btn-lg active p-2">Receipt</Link>
       </nav>
       <Routes>
         <Route path="/" element={<Main />} />
         <Route path="/provider" element={<Provider/>} />
         <Route path="/product" element={<Product/>} />
+        <Route path="/receipt" element={<Receipt/>} />
       </Routes>
       <Footer/>
     </div>
